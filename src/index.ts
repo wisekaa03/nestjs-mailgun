b7fee35ba3345b3432193cefd8ff850a5bcfbd3a
Stanislav Vyaliy <wisekaa03@gmail.com>
2021-12-05 00:01:39 +0300

    cosmetic fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { MailgunModule } from './mailgun.module';
 export type { EmailOptions, OptionsAsync } from './interfaces';
 export { MailgunService } from './mailgun.service';
-export { default as APIError } from 'mailgun.js/lib/error';
+export { default as APIError } from 'mailgun.js/dist/lib/error';
